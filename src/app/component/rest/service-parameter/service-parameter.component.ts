959ce7d0f4a2397f522d6450fe009b096dedf567
ajitbiswal <ajit1.biswal@gmail.com>
2023-12-28 20:16:39 +0530

    service parameter changes

--- src/app/component/rest/service-parameter/service-parameter.component.ts
+++ src/app/component/rest/service-parameter/service-parameter.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ServiceParameter } from '../service-parameter/service-parameter.service';
+import { ServiceParameterService } from '../service-parameter/service-parameter.service'
+import { Service } from 'src/app/entity/service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'src/app/http/message.service'; 
 
 @Component({
   selector: 'app-service-parameter',
@@ -7,9 +13,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceParameterComponent implements OnInit {
 
-  constructor() { }
+  private isOnboard = true;
+  submitButtonName = 'Submit';
+  originalService: Service[]=[];
+  service = {} as Service;
+  clientId:any;
+  serviceId:any;
 
-  ngOnInit(): void {
+  serviceParameterForm: FormGroup  ;
+  required: string[] = ['GET', 'POST', 'PUT', 'DELETE']; // Add more methods as needed
+  type: string[] = ['header','body']
+  paramtyp: string[] = ['string','int']
+  questionAsked: string[] = ['Yes','No']
+ 
+  //service:Service [] = [];
+  //responseType:string[] = [];
+  //requestType:string[] = [];
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute, 
+    private serviceParameterService: ServiceParameterService,  // Make sure this is included
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private serciceParameterService: ServiceParameterService) {
+     
+      this.clientId=localStorage.getItem('id');
+      alert('the client id is'+this.clientId);
+        this.serviceParameterForm = this.formBuilder.group({
+        id: ['0',Validators.required],
+        clientId: [this.clientId, [Validators.required]],
+        applicationId: ['', [Validators.required]],
+        method:['',Validators.required],
+        endpoint:['',Validators.required],
+        name:['',Validators.required],
+        keyword:['',Validators.required],
+        summary:['',Validators.required],
+        responseSchema:['',Validators.required],
+        botResponseTemplate:['',Validators.required],
+        requestTypes: [[]],
+        responseTypes: [[]],
+      });
+      
+ 
+      
+      
+
+      //this.f['responseTypes'].valueChanges.subscribe(v=>{
+       // this.responseType = v;
+      //});
+
+      //this.f['requestTypes'].valueChanges.subscribe(v=>{
+      //  this.requestType = v;
+      //});
+      
+
+    }
+
+    
+      ngOnInit(): void {
   }
 
+    get f() { return this.serviceParameterForm.controls; }
+    
+    
+
+  
+
+
+
+addParameter(serviceId:number){
+  alert(serviceId);
+  this.router.navigate(['main/parameter'],{ state: { id: serviceId } }) ;
 }
+ 
+
+
+onSubmit() {    
+  if (this.serviceParameterForm.invalid) { 
+    console.log('Form values:', this.serviceParameterForm.value);
+    alert('invalid input');
+    return;
+  }
+
+  this.serviceParameterService.onBoard(this.serviceParameterForm.value)
+    .subscribe(response => {
+      // Handle the response here
+      console.log('Service onBoard response:', response);
+    }, error => {
+      // Handle the error here
+      console.error('Error on onBoard service:', error);
+    });
+}
+
+  
+    
+  }
--- src/app/component/rest/service-parameter/service-parameter.service.ts
+++ src/app/component/rest/service-parameter/service-parameter.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UrlService } from 'src/app/common/url.service';
+import {  HandleError, HttpErrorHandlerService } from '../../../http/http-error-handler.service'
+import { Observable, catchError } from 'rxjs';
+import { ServiceParameter } from 'src/app/entity/serviceParameters';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceParameterService {
+  private handleError: HandleError;
+  private token:any;
+  private id:any;
+  constructor(private http: HttpClient,
+    private httpErrorHandler: HttpErrorHandlerService, 
+    private url : UrlService) {
+      this.handleError = httpErrorHandler.createHandleError('ServiceParameter');
+      this.token=localStorage.getItem('token');
+      this.id=localStorage.getItem('id');
+  }
 
-  constructor() { }
+
+  onBoard(serviceParameter:ServiceParameter) : Observable<string | any> {
+    const url = this.url.service();     
+    
+ 
+    const httpOptions = { headers: new HttpHeaders({ 'X-AUTH-LOG-HEADER':'sfsdfklksf-sfdfsf', 'Content-Type': 'application/json','accept':'application/json' }) };
+    return this.http.post<any>(url,serviceParameter,httpOptions)
+    .pipe(
+      catchError(this.handleError('OnBoard Service'))
+    )
+  }
+
+  
+
+
+ 
+  
+
 }
+export { ServiceParameter };
+
