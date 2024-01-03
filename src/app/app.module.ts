d41be1aa5d5201117cfd1bfb975c1acef29454c5
“jitendrasagoriya” <jitendra.sagoriya@outlook.com>
2024-01-03 23:15:47 +0530

    added notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,48 @@ import { UserComponent } from './component/rest/user/user.component';
 import { ClientDetailsComponent } from './component/rest/client-details/client-details.component';
 import { ForgotPasswordComponent } from './component/auth/forgot-password/forgot-password.component';
 import { ServiceLogComponent } from './component/rest/service-log/service-log.component'; 
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+
+const customNotifierOptions: NotifierOptions = {
+  position: {
+		horizontal: {
+			position: 'right',
+			distance: 12
+		},
+		vertical: {
+			position: 'top',
+			distance: 12,
+			gap: 10
+		}
+	},
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
 
 @NgModule({
   declarations: [
@@ -52,7 +94,7 @@ import { ServiceLogComponent } from './component/rest/service-log/service-log.co
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-
+    NotifierModule.withConfig(customNotifierOptions)
   ],
   providers: [],
   bootstrap: [AppComponent]
